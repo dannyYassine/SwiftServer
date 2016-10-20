b5cff9dc2834f6f6bf8a46b8060d7fed1e39a775
Danny Yassine <dannyyassine@gmail.com>
2016-10-20 01:27:03 -0400

    - initial commit

---
+++ Sources/home.swift
@@ -0,0 +1,22 @@
+//
+//  home.swift
+//  PerfectTemplate
+//
+//  Created by Danny Yassine on 2016-10-20.
+//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import Foundation
+
+func homeRoutes() -> Routes {
+    
+    var routes = Routes()
+    
+    
+    
+    return routes
+    
+}
